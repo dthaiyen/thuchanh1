6be40d48338c04e82b9946fdc2564cfac010785b
dthaiyen <70678828+dthaiyen@users.noreply.github.com>
2020-09-23 14:58:07 +0700

    change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'thuchanh1';
+export class AppComponent implements OnInit {
+  x: number;
+  n: number;
+  s = 0;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  bai1() {
+    this.s = 0;
+    let lt = 1;
+    for (let i = 1; i <= this.n; i++) {
+      lt *= this.x;
+      this.s += lt;
+    }
+  }
+
 }
